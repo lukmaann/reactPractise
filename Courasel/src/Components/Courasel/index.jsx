fbb3635c7915fc78b3c460cfedb18a50128cde85
LNUAKX <lukmaannadaf@gmail.com>
2024-03-25 13:41:25 +0530

    added automatic scroll effect

--- Courasel/src/Components/Courasel/index.jsx
+++ Courasel/src/Components/Courasel/index.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react/prop-types */
-import  { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CarouselItem from '../CouraselItem';
-
-
 
 const Carousel = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -18,17 +16,25 @@ const Carousel = ({ items }) => {
 
   useEffect(() => {
     if (carouselRef.current) {
-      const scrollAmount = carouselRef.current.scrollWidth / items.length * activeIndex - carouselRef.current.offsetWidth / 2+ (carouselRef.current.scrollWidth / items.length) / 2;
+      const scrollAmount = carouselRef.current.scrollWidth / items.length * activeIndex - carouselRef.current.offsetWidth / 2 + (carouselRef.current.scrollWidth / items.length) / 2;
       carouselRef.current.scrollTo({ left: scrollAmount, behavior: 'smooth' });
     }
   }, [activeIndex, items.length]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % items.length);
+    }, 1000); 
+
+    return () => clearInterval(interval);
+  }, [items.length]);
 
   return (
     <div className="relative flex items-center w-full justify-center py-6 ">
       <button onClick={handlePrev} className="absolute left-0 z-10 bg-orange-500 p-2 rounded-full shadow-md">
         Prev
       </button>
-      <div className="flex  w-[60%] gap-10 overflow-hidden" ref={carouselRef}>
+      <div className="flex w-[60%] gap-10 overflow-hidden" ref={carouselRef}>
         {items.map((item, index) => (
           <CarouselItem key={index} {...item} isActive={index === activeIndex} />
         ))}
@@ -39,6 +45,5 @@ const Carousel = ({ items }) => {
     </div>
   );
 };
-
 
 export default Carousel;
